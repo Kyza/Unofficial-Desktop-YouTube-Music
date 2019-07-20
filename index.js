8b6e7deb46da3ee1963a4109857cdf88eee8e70d
Kyza <TylerC2077@gmail.com>
2019-07-20 15:04:47 -0400

    The window now hides instead of closing like Spotify

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const {
   app,
-  BrowserWindow
-} = require('electron');
+  BrowserWindow,
+  Tray,
+  Menu
+} = require("electron");
 
-// Keep a global reference of the window object, if you don't, the window will
+const path = require("path");
+
+// Keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+let isQuiting;
+let tray;
+let trayMenu;
 
 function createWindow() {
   // Create the browser window.
@@ -15,12 +22,49 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     },
-		title: "YouTube Music",
-		icon: __dirname + "/images/favicon.png"
+    title: "YouTube Music",
+    icon: __dirname + "/images/favicon.png"
   });
 
-	win.setMenu(null);
-	win.maximize();
+
+  tray = new Tray(__dirname + "/images/favicon.png");
+  tray.setToolTip("YouTube Music");
+
+	tray.on("double-click", function() {
+		showWindow();
+	});
+
+  showWindow();
+
+  if (win.setThumbarButtons([{
+      tooltip: "Previous Song",
+      icon: __dirname + "/images/previous.png",
+      flags: ["enabled"],
+      click() {
+        console.log("Previous clicked.");
+      }
+    }, {
+      tooltip: "Play",
+      icon: __dirname + "/images/play.png",
+      flags: ["enabled"],
+      click() {
+        console.log("Play clicked.");
+      }
+    }, {
+      tooltip: "Next Song",
+      icon: __dirname + "/images/next.png",
+      flags: ["enabled"],
+      click() {
+        console.log("Next clicked.");
+      }
+    }])) {
+    console.log("Thumbbar buttons are supported.");
+  } else {
+    console.log("Thumbbar buttons are not supported.");
+  }
+
+  win.setMenu(null);
+  win.maximize();
 
   // and load the index.html of the app.
   win.loadURL("https://music.youtube.com/");
@@ -28,36 +72,79 @@ function createWindow() {
   // Open the DevTools.
   // win.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
+  win.on("close", function(event) {
+    if (!isQuiting) {
+      event.preventDefault();
+      hideWindow();
+      event.returnValue = false;
+    }
   });
+}
+
+function hiddenContextMenu() {
+  trayMenu = Menu.buildFromTemplate([{
+      label: "Open",
+      click: function() {
+        showWindow();
+      }
+    },
+    {
+      label: "Quit",
+      click: function() {
+        isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(trayMenu);
+}
+
+function shownContextMenu() {
+  trayMenu = Menu.buildFromTemplate([{
+    label: "Quit",
+    click: function() {
+      isQuiting = true;
+      app.quit();
+    }
+  }]);
+  tray.setContextMenu(trayMenu);
+}
+
+function showWindow() {
+	win.show();
+	shownContextMenu();
+}
+
+function hideWindow() {
+	win.hide();
+	hiddenContextMenu();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
+
+app.on("before-quit", function() {
+  isQuiting = true;
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
+app.on("activate", () => {
+  // On macOS it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
+// In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
